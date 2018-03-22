0ae6d8502611cd7c27b90829d48eacdaf20842ea
Valentin Thirion <vt@abakusitsolutions.eu>
2018-03-22 15:28:39 +0100

    [ADD] First version with no price on the website

---
+++ website_sale_no_price_process/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "eCommerce 'No-Price' Management",
+
+    'summary': """
+    """,
+
+    'description': """
+        eCommerce 'No-Price' Management
+        
+        This module changes the normal behaviour of the ecommerce website to a 'ask for quotation' management.
+
+    Basically, it removes prices, payment methods and so on.
+    But leaves the process normal except that the orders have to be sent by the internal Odoo users to the customer who creates the quote request.
+        
+        This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
+    """,
+
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+
+    'category': 'Sale',
+    'version': '10.0.1.0',
+
+    'depends': [
+        'website_sale'
+    ],
+
+    'data': [
+        'views/product.xml',
+    ],
+
+    'installable': True
+}
