e30d9f3d64c438b7438284d432ac5fca3c48e743
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-04-08 22:27:35 +0200

    [CHG] Filter out empty categories from normal and collapsible category menu

---
+++ website_sale_category_navigation/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "eCommerce Category navigation customization",
+
+    'summary': """ Hide categories with no items
+    """,
+
+    'author': "Paul Ntabuye Butera, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+
+    'category': 'Sale',
+    'version': '10.0.1.0',
+
+    'depends': [
+        'website_sale'
+    ],
+
+    'data': [
+        'views/website_templates.xml',
+    ],
+
+    'installable': True
+}
---
+++ website_sale_category_navigation/controllers/main.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from odoo import http, tools, _
+from odoo.http import request
+from odoo.addons.website.controllers.main import QueryURL
+from odoo.addons.website.models.website import slug
+from odoo.addons.website_sale.controllers.main import WebsiteSale, TableCompute, PPG, PPR
+from pprint import pformat
+logger = logging.getLogger(__name__)
+
+
+@http.route()
+class WebsiteSaleWithProductCountByCategory(WebsiteSale):
+    """
+    We override the WebsiteSale class in order to add our own salt to shop() method
+    NOTE: Odoo controller uses a *real* python class inheritance contrary to Odoo modules
+    """
+    @http.route([
+        '/shop',
+        '/shop/page/<int:page>',
+        '/shop/category/<model("product.public.category"):category>',
+        '/shop/category/<model("product.public.category"):category>/page/<int:page>'
+    ], type='http', auth="public", website=True)
+    def shop(self, page=0, category=None, search='', ppg=False, **post):
+        if ppg:
+            try:
+                ppg = int(ppg)
+            except ValueError:
+                ppg = PPG
+            post["ppg"] = ppg
+        else:
+            ppg = PPG
+
+        attrib_list = request.httprequest.args.getlist('attrib')
+        attrib_values = [map(int, v.split("-")) for v in attrib_list if v]
+        attributes_ids = set([v[0] for v in attrib_values])
+        attrib_set = set([v[1] for v in attrib_values])
+
+        domain = self._get_search_domain(search, category, attrib_values)
+
+        keep = QueryURL('/shop', category=category and int(category), search=search, attrib=attrib_list, order=post.get('order'))
+        pricelist_context = dict(request.env.context)
+        if not pricelist_context.get('pricelist'):
+            pricelist = request.website.get_current_pricelist()
+            pricelist_context['pricelist'] = pricelist.id
+        else:
+            pricelist = request.env['product.pricelist'].browse(pricelist_context['pricelist'])
+
+        request.context = dict(request.context, pricelist=pricelist.id, partner=request.env.user.partner_id)
+
+        url = "/shop"
+        if search:
+            post["search"] = search
+        if category:
+            category = request.env['product.public.category'].browse(int(category))
+            url = "/shop/category/%s" % slug(category)
+        if attrib_list:
+            post['attrib'] = attrib_list
+
+        categs = request.env['product.public.category'].search([('parent_id', '=', False)])
+        Product = request.env['product.template']
+
+        parent_category_ids = []
+        if category:
+            parent_category_ids = [category.id]
+            current_category = category
+            while current_category.parent_id:
+                parent_category_ids.append(current_category.parent_id.id)
+                current_category = current_category.parent_id
+
+        # IMPORTANT: Unpublished items are NOT filtered out
+        categs_product_count = {}
+        for cat in categs:
+            cat_domain = self._get_search_domain(search, cat, attrib_values)
+            categs_product_count[cat.name] = Product.search_count(cat_domain)
+            if categs_product_count[cat.name] > 0:
+                # Then we also check his children
+                for cat_child in cat.child_id:
+                    cat_domain = self._get_search_domain(search, cat_child, attrib_values)
+                    categs_product_count[cat_child.name] = Product.search_count(cat_domain)
+
+        product_count = Product.search_count(domain)
+        pager = request.website.pager(url=url, total=product_count, page=page, step=ppg, scope=7, url_args=post)
+        products = Product.search(domain, limit=ppg, offset=pager['offset'], order=self._get_search_order(post))
+
+        ProductAttribute = request.env['product.attribute']
+        if products:
+            # get all products without limit
+            selected_products = Product.search(domain, limit=False)
+            attributes = ProductAttribute.search([('attribute_line_ids.product_tmpl_id', 'in', selected_products.ids)])
+        else:
+            attributes = ProductAttribute.browse(attributes_ids)
+
+        from_currency = request.env.user.company_id.currency_id
+        to_currency = pricelist.currency_id
+        compute_currency = lambda price: from_currency.compute(price, to_currency)
+
+        values = {
+            'search': search,
+            'category': category,
+            'attrib_values': attrib_values,
+            'attrib_set': attrib_set,
+            'pager': pager,
+            'pricelist': pricelist,
+            'products': products,
+            'search_count': product_count,  # common for all searchbox
+            'bins': TableCompute().process(products, ppg),
+            'rows': PPR,
+            'categories': categs,
+            'categories_product_count': categs_product_count,
+            'attributes': attributes,
+            'compute_currency': compute_currency,
+            'keep': keep,
+            'parent_category_ids': parent_category_ids,
+        }
+        if category:
+            values['main_object'] = category
+        return request.render("website_sale.products", values)
