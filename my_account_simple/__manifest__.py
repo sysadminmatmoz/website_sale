7c6e7901a1906393df3edcf69c1e9ea853341907
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-06-06 16:29:33 +0200

    [ADD] Simplified Contact Details

---
+++ my_account_simple/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Contact Details Simplified",
+    'summary': "Remove extra fields from contact details",
+    'author': "AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Website',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'website_portal'
+    ],
+    'data': [
+        'views/portal_templates.xml',
+    ],
+    'installable': True
+}
