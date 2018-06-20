8dbd0cc6b24b24a36f9dff1fc28be8d7a739f59a
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-06-20 11:00:19 +0200

    [ADD] 4 custom Orders menu entries for Sale

---
+++ webshop_simple_orders_list/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Simple Webshop Orders List",
+    'summary': "Simplified orders list",
+    'author': "AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Website',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'webshop_simple',
+    ],
+    'data': [
+        'views/sale_order.xml',
+    ],
+    'installable': True
+}
