ee8d63ad96fff9bfff45b2f26c5ec26f386a76b7
Valentin Thirion <vt@abakusitsolutions.eu>
2018-06-20 11:13:40 +0200

    [ADD] Added module to repo, first version

---
+++ website_sale_order_label/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Sale Order Labels",
+
+    'summary': "Website Sale Order Labels",
+
+    'author': "AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'eCommerce',
+    'version': '10.0.1.0.0',
+
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/res_company.xml',
+        'reports/sale_order_label_report.xml',
+    ],
+
+    'installable': True
+}
