24248e45a1551bb653ef6af50662c0a53963fe11
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-06-26 08:39:00 +0200

    [ADD] Birthday promotion

---
+++ webshop_sale_birthday_promotion/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Birthday promotion discount",
+    'summary': "Give a promotion on user birthday",
+    'author': "AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Website',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/email_templates.xml',
+    ],
+    'installable': True
+}
---
+++ webshop_sale_birthday_promotion/models/res_partner.py
@@ -0,0 +1,48 @@
+import logging
+from datetime import datetime
+from pprint import pformat
+from odoo import models, api, fields
+from odoo.http import request
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class res_partner(models.Model):
+    _inherit = 'res.partner'
+
+    def send_birthday_email(self, ids=None, context=None):
+        partner_obj = self.env['res.partner']
+        mail_mail = self.env['mail.mail']
+        mail_ids = []
+        today = datetime.datetime.now()
+        today_month_day = '%-' + today.strftime('%m') + '-' + today.strftime('%d')
+        par_id = partner_obj.search([('customer', '=', True), ('birthdate', 'like', today_month_day)])
+        if par_id:
+            try:
+                for val in partner_obj.browse(par_id):
+                    email_from = val.email
+                    name = val.name
+                    subject = "Birthday Wishes"
+                    body = _("Hello %s,\n" %(name))
+                    body += _("\tWish you Happy Birthday\n")
+                    footer = _("Kind regards.\n")
+                    footer += _("%s\n\n" % val.company_id.name)
+                    mail_ids.append(mail_mail.create({
+                        'email_to': email_from,
+                        'subject': subject,
+                        'body_html': '<pre><span class="inner-pre" style="font-size: 15px">%s<br>%s</span></pre>' % (body, footer)
+                     }, context=context))
+                    mail_mail.send(mail_ids, context=context)
+            except Exception, e:
+                print "Exception", e
+        return None
+
+    @api.multi
+    def cron_promotion_mailing(self):
+        """ Send mass mailing to all registered portal user with the right flag on """
+        template = self.env.ref('website_sale_birthday_promotion.email_template_user_birthday')
+        for record in self:
+            _logger.debug("ABAKUS: Birthday omotion email sent")
+            self.env['mail.template'].browse(template.id).send_mail(record.id)
+        return
