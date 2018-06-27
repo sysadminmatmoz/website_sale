ec4f6e600ee109cc2a1d212b05d4820cd4faae2e
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-06-27 03:11:28 +0200

    [ADD] Cron job and template mail

--- website_sale_birthday_promotion/models/res_partner.py
+++ website_sale_birthday_promotion/models/res_partner.py
@@ -14,39 +14,28 @@ class ResPartner(models.Model):
     bday = fields.Integer('Birthday Day')
     bmonth = fields.Integer('Birthday Month')
 
-    def cron_birthday_promotion_mailing(self):
-        partner_obj = self.env['res.partner']
-        mail_mail = self.env['mail.mail']
-        mail_ids = []
-        today = datetime.datetime.now()
-        par_id = partner_obj.search([
-            ('active', '=', True),
-            ('bday', '=', int(today.strftime('%d'))), ('bmonth', '=', int(today.strftime('%m')))])
-        if par_id:
-            try:
-                for val in partner_obj.browse(par_id):
-                    email_to = val.email
-                    name = val.name
-                    subject = "Birthday Promotion"
-                    body = _("Hello %s,\n" %(name))
-                    body += _("\tWish you Happy Birthday\n")
-                    footer = _("Kind regards.\n")
-                    footer += _("%s\n\n" % val.company_id.name)
-                    mail_ids.append(mail_mail.create({
-                        'email_to': email_to,
-                        'subject': subject,
-                        'body_html': '<pre><span class="inner-pre" style="font-size: 15px">%s<br>%s</span></pre>' % (body, footer)
-                     }, context=None))
-                    mail_mail.send(mail_ids, context=None)
-            except Exception, e:
-                print "Exception", e
-        return None
-
     @api.multi
     def cron_promotion_mailing(self):
         """ Send mass mailing to all registered portal user with the right flag on """
+        context = {}
+        if self.env.context:
+            context.update(self.env.context)
+        context['promo'] = {}
+        company = self.env.res_user.company
+        if not company.birthday_promotion_product_id:
+            return
+        context['promo'] = {
+            'product_gift': company.birthday_promotion_product_id.name,
+        }
+        self.env.context = context
         template = self.env.ref('website_sale_birthday_promotion.email_template_user_birthday')
-        for record in self:
-            _logger.debug("ABAKUS: Birthday omotion email sent")
-            self.env['mail.template'].browse(template.id).send_mail(record.id)
+        today = datetime.datetime.now()
+        public_users = self.env['res.users'].search([
+            ('partner_id.bday', '=', int(today.strftime('%d'))),
+            ('partner_id.bmonth', '=', int(today.strftime('%m')))
+        ])
+        for user in public_users:
+            if user.has_group('base.public_user'):
+                _logger.debug("ABAKUS: Birthday omotion email sent")
+                template.send_mail(user.id, force_send=True)
         return
