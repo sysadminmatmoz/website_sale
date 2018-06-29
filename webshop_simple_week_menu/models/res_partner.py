4e7baa1d95465c0e2c96c47e2a457c844f6ebda9
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-06-29 12:59:54 +0200

    [FIX] Cron run of week menu

--- webshop_simple_week_menu/models/res_partner.py
+++ webshop_simple_week_menu/models/res_partner.py
@@ -15,21 +15,31 @@ class ResPartner(models.Model):
     @api.multi
     def cron_week_menu_mailing(self):
         """ Send mass mailing to all registered portal users """
+        _logger.debug("ABAKUS: Week Menu Mailing - START")
         context = {}
         if self.env.context:
             context.update(self.env.context)
         context['promo'] = {}
         company = self.env.user.company_id
         if not company.week_menu_product_id:
+            _logger.debug("ABAKUS: Week Menu Mailing - NO WEEK MENU SET - STOP")
             return
+        _logger.debug("ABAKUS: Week Menu Mailing - Menu: {}".format(company.week_menu_product_id.name))
         context['promo'] = {
             'product_gift': company.week_menu_product_id.name,
         }
         self.env.context = context
-        template = self.env.ref('website_sale_week_menu.email_template_week_menu')
+        template = self.env.ref('webshop_simple_week_menu.email_template_week_menu')
         public_users = self.env['res.users'].search([])
+        _logger.debug("ABAKUS: Week Menu Mailing - Users num: {}".format(len(public_users)))
         for user in public_users:
             if user.has_group('base.group_portal'):
                 if user.partner_id.allow_week_menu_notification:
+                    _logger.debug("ABAKUS: Week Menu Mailing - SEND TO: {}".format(user.name))
                     template.send_mail(user.id, force_send=True)
+                else:
+                    _logger.debug("ABAKUS: Week Menu Mailing - DISABLED FOR: {}".format(user.name))
+            else:
+                _logger.debug("ABAKUS: Week Menu Mailing - SKIP USER: {}".format(user.name))
+
         return
