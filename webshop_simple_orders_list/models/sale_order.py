c86701b63bd617a98c892cfefe02898176d9100c
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-07-10 07:56:49 +0200

    [CHG] Today orders list based on delivery date

--- webshop_simple_orders_list/models/sale_order.py
+++ webshop_simple_orders_list/models/sale_order.py
@@ -13,40 +13,6 @@ class SaleOrder(models.Model):
 
     payment_state = fields.Selection(related='payment_tx_id.state')
 
-    def filter_today_action(self):
-        # check company_id
-        company_id = self.env.user.company_id
-        # NOTE: use timezone when fetching today
-        today = datetime.now(pytz.timezone(company_id.openhours_tz or 'GMT'))
-        yesterday = today - timedelta(days=1)
-        from_date = "{} {:02d}:{:02d}:00".format(
-            yesterday.strftime('%Y-%m-%d'),
-            int(company_id.openhours_open),
-            int(company_id.openhours_open % 1 * 60))
-        to_date = "{} {:02d}:{:02d}:00".format(
-            today.strftime('%Y-%m-%d'),
-            int(company_id.openhours_close),
-            int(company_id.openhours_close % 1 * 60))
-        tree_id = self.env.ref("webshop_simple_orders_list.view_simple_orders_tree")
-        form_id = self.env.ref("webshop_simple_orders_list.view_simple_orders_form")
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'Today\'s Orders',
-            'view_type': 'form',
-            'view_mode': 'tree,form',
-            'res_model': 'sale.order',
-            'domain': [
-                ('state', 'in', ['sale', 'done']),
-                ('payment_state', '=', 'done'),
-                ('confirmation_date', '>=', from_date),
-                ('confirmation_date', '<=', to_date)],
-            # if you don't want to specify form for example
-            # (False, 'form') just pass False
-            'views': [(tree_id.id, 'tree'), (form_id.id, 'form')],
-            'target': 'current',
-            'context': None,
-        }
-
     def filter_tomorrow_action(self):
         # check company_id
         company_id = self.env.user.company_id
