5caaded5b31b239639ce11d64e0d0682f2788ca4
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-09-07 03:02:34 +0200

    [ADD] Menu of the day visual hint

--- webshop_simple_week_menu/models/res_company.py
+++ webshop_simple_week_menu/models/res_company.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # (c) AbAKUS IT Solutions
+import pytz
+import datetime
 import logging
 from odoo import api, fields, models, _
 
@@ -14,3 +16,18 @@ class ResCompany(models.Model):
     wednesday_product_id = fields.Many2one('product.product', string='Wednesday Product')
     thursday_product_id = fields.Many2one('product.product', string='Thursday Product')
     friday_product_id = fields.Many2one('product.product', string='Friday Product')
+
+    def get_named_day_product(self):
+        week_day = datetime.datetime.now(pytz.timezone('Europe/Brussels')).weekday()
+        if week_day == 0:
+            return self.monday_product_id
+        elif week_day == 1:
+            return self.tuesday_product_id
+        elif week_day == 2:
+            return self.wednesday_product_id
+        elif week_day == 3:
+            return self.thursday_product_id
+        elif week_day == 4:
+            return self.friday_product_id
+        else:
+            return None
