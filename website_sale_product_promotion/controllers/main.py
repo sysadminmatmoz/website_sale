77f324883b33519cd698cdf3a37cee2dbfd200f1
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-09-07 09:03:23 +0200

    [ADD] Export promo info to category page

--- website_sale_product_promotion/controllers/main.py
+++ website_sale_product_promotion/controllers/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
-
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSaleSimple
 from odoo.addons.website_portal.controllers.main import website_account
 website_account.OPTIONAL_BILLING_FIELDS += ['product_promotion_notification']
 
@@ -18,3 +19,20 @@ class WebsiteAccountPromoNotification(website_account):
         else:
             data['product_promotion_notification'] = False
         return super(WebsiteAccountPromoNotification, self).details_form_validate(data)
+
+
+class WebsiteSimplePromos(WebsiteSaleSimple):
+
+    @staticmethod
+    def shop_simple_category_render(page, values):
+        # Get all promo product from this week
+        promo_record = request.env['product.promotion'].search([('state', '=', 'curr')], limit=1)
+        if promo_record:
+            promo_products = {
+                'sandwich': promo_record.product_sandwich,
+                'sandwich_promo_price': promo_record.product_sandwich_promo_price,
+                'salad': promo_record.product_salad,
+                'salad_promo_price': promo_record.product_salad_promo_price,
+            }
+            values['promo_products'] = promo_products
+        return request.render(page, values)
